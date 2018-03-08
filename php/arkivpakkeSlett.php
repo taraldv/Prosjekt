901291fcd229eb62c8c1ab0e91dc44badfca27b6
taraldv <36439345+taraldv@users.noreply.github.com>
2018-03-08 18:48:48 +0100

    arkivpakkefil slettes sammen med selve arkivpakken

--- php/arkivpakkeSlett.php
+++ php/arkivpakkeSlett.php
@@ -3,7 +3,19 @@ require_once 'hjelpeFunksjoner.php';
 session_start();
 if(isset($_SESSION['brukernavn'])){
 	$arkivpakkeID = $_POST['arkivpakkeID'];
+
+	$filNavnQuery = 'SELECT d.filID,d.filnavn FROM arkivpakke a INNER JOIN doklager d ON a.dokfil = d.filID WHERE a.arkivID = ?';
+	$filResult = databaseKobling($filNavnQuery,'i',array($arkivpakkeID));
+	$filId = $filResult[0]['filID'];
+	$filnavn = $filResult[0]['filnavn'];
+	$dir = '/home/skule/doklager/';
+	$file = "$dir$filId$filnavn";
 	$query = 'DELETE FROM arkivpakke WHERE arkivID = ?';
-	echo databaseKobling($query,"i",array($arkivpakkeID));
+
+	if(unlink($file)){
+		echo databaseKobling($query,'i',array($arkivpakkeID));
+	} else {
+		echo 0;
+	}
 }
 ?>
