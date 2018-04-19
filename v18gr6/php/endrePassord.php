0216c029784f206bb3b132b4d895376a3fb53d77
taraldv <36439345+taraldv@users.noreply.github.com>
2018-04-19 15:05:40 +0200

    mer xampp errors

--- v18gr6/php/endrePassord.php
+++ v18gr6/php/endrePassord.php
@@ -4,16 +4,16 @@ session_start();
 if(isset($_SESSION['brukernavn'])){
 	$brukernavn = $_SESSION['brukernavn'];
 	$gammeltPassord = $_POST['gammeltPassord'];
-	$nyttPassord = $_POST['nyttPassord'];
 	$query = 'SELECT passord,fornavn,etternavn FROM bruker WHERE brukernavn= ?';
 	$result = databaseKobling($query,'s',array($brukernavn));
 	$hash = $result[0]['passord'];
 	$boolean = password_verify($gammeltPassord, $hash);
 	//Sjekker om nyttPassord ble sendt som parameter i POST
-	if (!isset($nyttPassord)) {
+	if (!isset($_POST['nyttPassord'])) {
 		//Sjekker om gammelt passord fra POST er korrekt
 		echo $boolean;
-	} else if ($boolean) {
+	} else if ($boolean && isset($_POST['nyttPassord'])) {
+		$nyttPassord = $_POST['nyttPassord'];
 		//Oppdaterer databasen med nytt passord, hvis det gamle er korrekt.
 		$query2 = 'UPDATE bruker SET passord = ? WHERE brukernavn= ?';
 		echo databaseKobling($query2,'ss',array(password_hash($nyttPassord, PASSWORD_DEFAULT),$brukernavn));
